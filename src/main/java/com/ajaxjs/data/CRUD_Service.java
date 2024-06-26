9cf335637ca5fb2fd2d50694f5e693fdbe48b2f8
sp42@qq.com <sp42@qq.com>
2024-06-26 17:32:45 +0800

    ci

--- src/main/java/com/ajaxjs/data/CRUD_Service.java
+++ src/main/java/com/ajaxjs/data/CRUD_Service.java
@@ -1,20 +1,23 @@
 package com.ajaxjs.data;
 
+import com.ajaxjs.data.data_service.DataServiceUtils;
 import com.ajaxjs.data.jdbc_helper.JdbcReader;
 import com.ajaxjs.data.jdbc_helper.JdbcWriter;
 import com.ajaxjs.data.jdbc_helper.common.IdField;
 import com.ajaxjs.data.jdbc_helper.common.TableName;
 import com.ajaxjs.util.ListUtils;
+import com.ajaxjs.util.reflect.Methods;
 import lombok.Data;
 import lombok.experimental.Accessors;
 import org.springframework.util.StringUtils;
 
+import java.io.Serializable;
 import java.util.List;
 import java.util.Map;
 
 @Data
 @Accessors(chain = true)
-public class CRUD_Service<T> {
+public class CRUD_Service implements DataAccessObject {
     private JdbcReader reader;
 
     private JdbcWriter writer;
@@ -24,114 +27,169 @@ public class CRUD_Service<T> {
      */
     private SmallMyBatis smallMyBatis;
 
-    /**
-     * SQL XML 里的 id
-     */
-    private String sqlId;
-
-    /**
-     * SQL 语句
-     */
-    private String sql;
-
-    private Map<String, Object> mapParams;
-
-    private Class<T> beanClz;
-
-    private Object[] orderedParams;
-
-    /**
-     * 获取 SQL 语句，如果是 SQL Id 则获取并解析之
-     *
-     * @return 最终 SQL 语句
-     */
-    private String getRealSql() {
-        if (StringUtils.hasText(sql))
-            return sql;
-        else if (StringUtils.hasText(sqlId))
-            return smallMyBatis.handleSql(mapParams, sqlId);
-        else throw new IllegalArgumentException("没输入的 SQL 参数");
+    @Override
+    public <T> T queryOne(Class<T> clz, String sql, Object... params) {
+        return reader.queryOne(sql, clz, params);
     }
 
-    /**
-     * 获取单笔详情
-     *
-     * @return 可能是 Map 或者 Java Bean，根据 beanClz 是否有值来决定
-     */
-    public Object info() {
-        String sql = getRealSql();
-
-        return beanClz == null ? reader.queryAsMap(sql, orderedParams) : reader.queryAsBean(beanClz, sql, orderedParams);
+    @Override
+    public <T> T info(Class<T> beanClz, String sql, Object... params) {
+        return reader.queryAsBean(beanClz, sql, params);
     }
 
-    @SuppressWarnings("unchecked")
-    public T infoBean() {
-        Object info = info();
-        if (info == null)
-            return null;
+    @Override
+    public <T> T infoBySqlId(Class<T> beanClz, String sqlId, Map<String, Object> mapParams, Object... params) {
+        String sql = smallMyBatis.handleSql(mapParams, sqlId);
 
-        return (T) info;
+        return reader.queryAsBean(beanClz, sql, params);
     }
 
-    @SuppressWarnings("unchecked")
-    public Map<String, Object> infoMap() {
-        Object info = info();
-        if (info == null)
-            return null;
-
-        return (Map<String, Object>) info;
+    @Override
+    public Map<String, Object> infoMap(String sql, Object... params) {
+        return reader.queryAsMap(sql, params);
     }
 
-    /**
-     * 执行查询操作并返回查询结果。
-     * <p>
-     * 本方法根据 beanClz 是否为 null 来决定查询结果的返回类型。如果 beanClz 为 null，则查询结果将作为 Map 列表返回；
-     * 否则，查询结果将作为指定 bean 类的实例列表返回。这提供了一种灵活的方式，根据调用方的需求返回不同类型的结果。
-     *
-     * @return 查询结果列表，可以是任何类型的列表，具体类型取决于 beanClz 的值。
-     */
-    public List<?> list() {
-        // 获取实际的SQL语句，这可能涉及到动态拼接 SQL 或其他逻辑。
-        String sql = getRealSql();
+    @Override
+    public Map<String, Object> infoMapBySqlId(String sqlId, Map<String, Object> mapParams, Object... params) {
+        String sql = smallMyBatis.handleSql(mapParams, sqlId);
 
-        // 根据 beanClz 是否为 null，选择不同的查询方法，并返回查询结果。
-        // 如果 beanClz 为 null，调 用queryAsMapList 方法，返回 Map 列表；否则，调用 queryAsBeanList 方法，返回指定 bean 类的实例列表。
-        return beanClz == null ? reader.queryAsMapList(sql, orderedParams) : reader.queryAsBeanList(beanClz, sql, orderedParams);
+        return reader.queryAsMap(sql, params);
     }
 
-    /**
-     * 将查询结果转换为指定类型的Bean列表。
-     * <p>
-     * 本方法通过类型转换将查询到的数据转换为泛型列表。由于Java的类型擦除，这里需要使用 @SuppressWarnings 注解来抑制编译器警告。
-     * 方法的核心在于将查询结果（假设为List类型）强制转换为泛型 List<T> 类型，以便返回给调用者一个类型安全的列表。
-     * 如果查询结果为空或列表内元素类型不匹配，将会在运行时抛出异常。
-     *
-     * @return 返回一个泛型列表，包含指定类型的Bean对象。如果查询结果为空，则返回一个空列表，而不是null。
-     */
-    @SuppressWarnings("unchecked")
-    public List<T> listBean() {
-        // 通过类型转换将查询结果转换为泛型List<T>类型
-        List<T> list = (List<T>) list();
-
-        // 使用ListUtils的getList方法，确保返回的列表不为null，如果原始列表为null，则返回一个空列表
-        return ListUtils.getList(list);
+    @Override
+    public <T> List<T> list(Class<T> beanClz, String sql, Object... params) {
+        return ListUtils.getList(reader.queryAsBeanList(beanClz, sql, params));
     }
 
-    /**
-     * 将查询结果转换为 List<Map<String, Object>> 类型。
-     * 本方法假设调用了一个未在代码中显示的方法list()，该方法返回一个对象，
-     * 此对象需要通过类型转换变为 List<Map<String, Object>> 类型。
-     * 使用@SuppressWarnings注解来抑制编译器对类型转换的警告。
-     *
-     * @return 转换后的List<Map < String, Object>>对象，如果原始结果为空或转换不可行，则返回一个空列表。
-     */
-    @SuppressWarnings("unchecked")
-    public List<Map<String, Object>> listMap() {
-        // 将list()方法返回的对象强制转换为List<Map<String, Object>>类型
-        List<Map<String, Object>> list = (List<Map<String, Object>>) list();
+    @Override
+    public <T> List<T> listById(Class<T> beanClz, String sqlId, Map<String, Object> mapParams, Object... params) {
+        String sql = smallMyBatis.handleSql(mapParams, sqlId);
 
-        // 使用ListUtils的getList方法确保返回的列表不为空，如果原始列表为空，则返回一个空列表
-        return ListUtils.getList(list);
+        return ListUtils.getList(reader.queryAsBeanList(beanClz, sql, params));
+    }
+
+    @Override
+    public List<Map<String, Object>> listMap(String sql, Object... params) {
+        return ListUtils.getList(reader.queryAsMapList(sql, params));
+    }
+
+    @Override
+    public List<Map<String, Object>> listMapBySqlId(String sqlId, Map<String, Object> mapParams, Object... params) {
+        String sql = smallMyBatis.handleSql(mapParams, sqlId);
+
+        return ListUtils.getList(reader.queryAsMapList(sql, params));
+    }
+
+    @Override
+    public <T> PageResult<T> page(Class<T> beanClz, String sql, Map<String, Object> paramsMap) {
+        sql = SmallMyBatis.handleSql(sql, paramsMap);
+
+        PageEnhancer p = new PageEnhancer();
+        p.setJdbcReader(reader);
+        p.initSql(sql, DataServiceUtils.getRequest());
+
+        return p.page(beanClz);
+    }
+
+    @Override
+    public <T> PageResult<T> pageBySqlId(Class<T> beanClz, String sqlId, Map<String, Object> mapParams) {
+        String sql = smallMyBatis.handleSql(mapParams, sqlId);
+
+        PageEnhancer p = new PageEnhancer();
+        p.setJdbcReader(reader);
+        p.initSql(sql, DataServiceUtils.getRequest());
+
+        return p.page(beanClz);
+    }
+
+    @Override
+    public Long create(String talebName, Object entity, String idField) {
+        writer.setTableName(talebName);
+
+        if (StringUtils.hasText(idField))
+            writer.setIdField(idField); // 如果ID字段名不为空，则设置 ID 字段名
+
+        return (Long) writer.create(entity);
+    }
+
+    @Override
+    public Long createWithIdField(Object entity, String idField) {
+        return create(getTableName(entity), entity, idField);
+    }
+
+    @Override
+    public Long createWithIdField(Object entity) {
+        return createWithIdField(entity, getIdField(entity));
+    }
+
+    @Override
+    public Long create(Object entity) {
+        return create(getTableName(entity), entity, null);
+    }
+
+    @Override
+    public boolean update(String talebName, Object entity, String idField) {
+        writer.setTableName(talebName);
+
+        if (StringUtils.hasText(idField))
+            writer.setIdField(idField);
+        else throw new DataAccessException("未指定 id，这将会是批量全体更新！");
+
+        return writer.update(entity) > 0;
+    }
+
+    @Override
+    public boolean update(String talebName, Object entity) {
+        return update(talebName, entity, null);
+    }
+
+    @Override
+    public boolean update(Object entity) {
+        return update(getTableName(entity), entity);
+    }
+
+    @Override
+    public boolean updateWithIdField(Object entity) {
+        return updateWithIdField(entity, getIdField(entity));
+    }
+
+    @Override
+    public boolean updateWithIdField(Object entity, String idField) {
+        return update(getTableName(entity), entity, idField);
+    }
+
+    @Override
+    public boolean updateWithWhere(Object entity, String where) {
+        String talebName = getTableName(entity);
+
+        writer.setTableName(talebName);
+        writer.setWhere(where);
+
+        return writer.updateWhere(entity, where) > 0;
+    }
+
+    @Override
+    public boolean delete(Object entity, Serializable id) {
+        return delete(getTableName(entity), id);
+    }
+
+    @Override
+    public boolean delete(String talebName, Serializable id) {
+        writer.setTableName(talebName);
+
+        return writer.delete(id);
+    }
+
+    @Override
+    public boolean delete(Object entity) {
+        Object id = Methods.executeMethod(entity, "getId");
+
+        if (id != null) {
+            return delete(entity, (Serializable) id);
+        } else {
+            System.err.println("没有 getId()");
+            return false;
+        }
     }
 
     /**
@@ -142,7 +200,8 @@ public class CRUD_Service<T> {
      */
     public static String getTableName(Object entity) {
         TableName tableNameA = entity.getClass().getAnnotation(TableName.class);
-        if (tableNameA == null) throw new RuntimeException("实体类未提供表名");
+        if (tableNameA == null)
+            throw new RuntimeException("实体类未提供表名");
 
         return tableNameA.value();
     }
@@ -156,7 +215,8 @@ public class CRUD_Service<T> {
     public static String getIdField(Object entity) {
         IdField annotation = entity.getClass().getAnnotation(IdField.class);
 
-        if (annotation == null) throw new DataAccessException("没设置 IdField 注解，不知哪个主键字段");
+        if (annotation == null)
+            throw new DataAccessException("没设置 IdField 注解，不知哪个主键字段");
 
         return annotation.value();
     }
